2b50b00ae23f9ece425755f8af8324e15f998d7d
J-scar <jboss17@hotmail.com>
2022-03-14 23:21:32 -0800

    Update querys.js

--- DB/querys.js
+++ DB/querys.js
@@ -1,3 +1,4 @@
+const fetch = require('node-fetch');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const finnhub = require('finnhub');
 const api_key = finnhub.ApiClient.instance.authentications['api_key'];
@@ -7,14 +8,19 @@ api_key.apiKey = "c84b3jqad3ide9hei860"
 const finnhubClient = new finnhub.DefaultApi()
 const input = 'BLUEACACIA LTD - CLASS A'
 
-//test()
+test()
 async function test(){
     try {
-        await DataPrice(input)
-        await DataStock(input)
-        await DataType("ADR")
-        await UserData("001", "Pass")
-        await DataPriceSpan("INTL BUSINESS MACHINES CORP", "D", '02/13/2019 23:31:30', '02/13/2020 23:31:30')
+        let Data2 = await DataPrice(input)
+        console.log(Data2)
+        let Data3 = await DataStock(input)
+        //console.log(Data3)
+        let Data4 = await DataType("ADR")
+        //console.log(Data4)
+        let Data5 = await UserData("001", "Pass")
+        //console.log(Data5)
+        let Data6 = await DataPriceSpan("INTL BUSINESS MACHINES CORP", "D", '02/13/2019 23:31:30', '02/13/2020 23:31:30')
+        console.log(Data6)
     } finally {
         //Ensures that the client will close when you finish/error
         await client.close();
@@ -28,10 +34,13 @@ async function DataPrice(name) {
     const stock = database.collection('stock');
     const query = {description: name};
     const test = await stock.findOne(query, { projection: { _id: 0, symbol: 1 } });
-    console.log(test.symbol);
+    //console.log(test.symbol);
+    var ret = 0;
     finnhubClient.quote(test.symbol, (error, data, response) => {
         console.log(data)
+        ret = data;
     });
+    return ret;
 }
 
 async function DataPriceSpan(name, span, T1, T2) {
@@ -40,14 +49,16 @@ async function DataPriceSpan(name, span, T1, T2) {
     const stock = database.collection('stock');
     const query = {description: name};
     const test = await stock.findOne(query, { projection: { _id: 0, symbol: 1 } });
-    console.log(test.symbol);
     var datum = Date.parse(T1);
     var UNIXDate = datum/1000;
     var datum2 = Date.parse(T2);
     var UNIXDate2 = datum/1000;
-    finnhubClient.stockCandles("IBM", span, UNIXDate, UNIXDate2, (error, data, response) => {
+    const Url = 'https://finnhub.io/api/v1/stock/candle?symbol=${test.symbol}&resolution=${span}&from=${UNIXDate}&to=${UNIXDate2}&token=c84b3jqad3ide9hei860'
+    const response = await fetch(Url);
+    finnhubClient.stockCandles(test.symbol, span, UNIXDate, UNIXDate2, (error, data, response) => {
         console.log(data)
     });
+    return response;
 }
 
 async function DataStock(name) {
@@ -57,8 +68,8 @@ async function DataStock(name) {
     const stock = database.collection('stock');
     const query = {description: name};
     const test = await stock.findOne(query);
-    console.log(test);
-    
+    //console.log(test);
+    return test;
 }
 
 async function UserData(UID, Password) {
@@ -67,7 +78,8 @@ async function UserData(UID, Password) {
     const stock = database.collection('User');
     const query = {UID: UID, Password: Password};
     const test = await stock.findOne(query);
-    console.log(test);
+    //console.log(test);
+    return test;
 
 }
 
@@ -78,6 +90,6 @@ async function DataType(type) {
     const stock = database.collection('stock');
     const query = {type: type};
     const test = await stock.find(query).toArray();
-    console.log(test);
-  
+    //console.log(test);
+    return test;
 }
