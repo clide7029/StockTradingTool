1ccba9cdcf76ce740d4a8d0da6d8b12ea6c031f7
aanders7 <47235833+aanders7@users.noreply.github.com>
2022-03-20 15:27:44 -0700

    uploaded user files

---
+++ pages/user/Users.js
@@ -0,0 +1,25 @@
+
+import 'mongoose';
+//import 'bcrypt';
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    default: ''
+  },
+  password: {
+    type: String,
+    default: ''
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false
+  },
+  signUpDate: {
+    type: Date,
+    default: Date.now()
+  }
+});UserSchema.methods.generateHash = function(password) {
+  //return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};UserSchema.methods.validPassword = function(password) {
+  //return bcrypt.compareSync(password, this.password);
+};module.exports = mongoose.model('User', UserSchema);
