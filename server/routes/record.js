e667a9e5b3539bfd624f13d35594ac36aa7b68ed
Anders <aanders@pugetsound.edu>
2022-03-21 17:24:33 -0700

    Update record.js

--- server/routes/record.js
+++ server/routes/record.js
@@ -50,7 +50,7 @@ recordRoutes.route("/record/name/:name").get(function (req, res) {
 
 recordRoutes.route("//user/:username&:password").get(function (req, res) {
     let db_connect = dbo.getDb("stockData");
-    let myquery = { username: req.params.username,  Password: req.params.Password};
+    let myquery = { username: req.params.username,  password: req.params.password};
     db_connect
         .collection("User")
         .findOne(myquery, function (err, result) {
