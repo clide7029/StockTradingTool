acc0b0e199c0e45337c1603fc2b9cbedb0e32504
J-scar <jboss17@hotmail.com>
2022-03-22 14:10:48 -0800

    newest querys

---
+++ pages/api/user/[UID]/[Passw].js
@@ -0,0 +1,11 @@
+import { connectToDatabase } from "../../../../util/mongodb.js";
+export default async ({ query: { UID, Passw}}, res) => {
+  const { db } = await connectToDatabase();
+  const Users = await db
+    .collection("User")
+    .findOne({UID: UID, Password: Passw})
+    
+  res.json(Users);
+  console.log(UID)
+  console.log(Passw)
+};
