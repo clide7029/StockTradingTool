7a23edd2e04a752828ed04bf349facbd278cc406
Anders <aanders@pugetsound.edu>
2022-04-28 23:03:00 -0700

    Update changepassword.js

--- pages/api/user/changepassword.js
+++ pages/api/user/changepassword.js
@@ -1,7 +1,7 @@
 import { getSession } from 'next-auth/client';
 
 import { hashPassword, verifyPassword } from '../../lib/auth';
-import { connectToDatabase } from '../../lib/mongodb';
+import { connectToDatabase } from '../../../lib/mongodb';
 
 async function handler(req, res) {
   if (req.method !== 'PATCH') {
